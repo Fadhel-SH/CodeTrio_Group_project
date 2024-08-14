7e6dcf09453cb7f6f64fd25ad9894d0d03bb8924
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-14 12:08:20 +0300

    Set up routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
+      <Footer />
     </Router>
   );
 }
